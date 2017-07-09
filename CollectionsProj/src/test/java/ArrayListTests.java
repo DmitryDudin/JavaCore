ecf116aa250d3b07a02940fbeaf69832fbc1a779
Dmitry Dudin <dudin_dmitry@mail.ru>
2017-07-09 23:57:43 +0300

    Random

--- CollectionsProj/src/test/java/ArrayListTests.java
+++ CollectionsProj/src/test/java/ArrayListTests.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ArrayListTests {
     private ArrayList<Integer> preparedCollection;
@@ -12,10 +13,14 @@ public class ArrayListTests {
         for (int i = 0; i < 1000; i++) {
             preparedCollection.add(new Integer((int) (Math.random() * 1000)));
         }
+        Random randomGenerator = new Random();
+        randomGenerator.ints(10, 10, 30).forEach(i -> System.out.println(i));
+        
+
     }
 
     @Test
-    public void testAddPerformance(){
+    public void testAddPerformance() {
 
 
     }
