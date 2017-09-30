bb1226b604302069ab7a64b5904e33223aace1a9
Dmitry Dudin <dudin_dmitry@mail.ru>
2017-09-30 21:57:11 +0300

    ww Java8StreamAPI

--- Java8StreamAPI/src/test/java/UserStreamTests.java
+++ Java8StreamAPI/src/test/java/UserStreamTests.java
@@ -2,6 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -138,7 +141,7 @@ public class UserStreamTests {
     @Test
     public void peekTest() {
 //        Stream<T> peek(Consumer<? super T> action);
-
+//peek - применяется для выполнения промежуточных действий над стримом, как поавилло используют при отладке (sout)
         List<User> sorted = new LinkedList<>();
         List<User> users = Arrays.asList(new User(1l, "Nika", User.UserrRole.USER),
                 new User(2l, "Mike", User.UserrRole.USER),
@@ -158,6 +161,189 @@ public class UserStreamTests {
         System.out.println(sorted);
 
     }
-//    54min
+
+    @Test
+    public void forEachTest() {
+//        forEach - terminal operation
+//        forEachOrdered - terminal operation соблюдает порядок выполнения, чуть медленее
+        List<String> names = new LinkedList<>();
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        users.stream()
+                .map(user -> user.getName())
+                .forEach(name -> names.add(name));
+        System.out.println(names);
+    }
+
+    @Test
+    public void mapTest() {
+//        map - функция высшего порядка, кот означает применить ко всем
+//        результатом её выполнения будет другой stream из N элементов
+        List<String> names = new LinkedList<>();
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        users.stream()
+                .map(user -> user.getName())
+                .forEach(name -> names.add(name));
+        System.out.println(names);
+
+        List<Long> ids = users.stream()
+                .map(User::getId)
+                .collect(toList());
+        System.out.println(ids);
+    }
+
+    @Test
+    public void mapToLongTest() {
+//        mapToLong -  LongStream has methods min, max, average, sum, summaryStatistics etc.
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        long[] ids = users.stream()
+                .mapToLong(user -> user.getId())
+                .toArray();
+        System.out.println(Arrays.toString(ids));
+        long sum = users.stream()
+                .mapToLong(user -> user.getId())
+                .sum();
+        System.out.println(sum);
+
+        OptionalDouble average = users.stream()
+                .mapToLong(user -> user.getId())
+                .average();
+        System.out.println(average.getAsDouble());
+    }
+//    аналогично:
+//    -mapToInt
+//    -mapToDouble
+//    наличие специфических методов для работы с числовыми стримами
+
+//    -mapToObj - позволяет числовой стрим преобразовать в стрим обьектов
+
+
+    @Test
+    public void flatMapTest() {
+//        когда необходимо некое дерево развернуть в плоскую структуру
+//                для этого нужна Function принимающая обьект "стрима"
+//                и возвращающая "стрим" результатов
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        List<String> flatMap = users.stream()
+                .flatMap(user -> Stream.of(
+                        "id=" + user.getId(),
+                        "name=" + user.getName(),
+                        "role" + user.getRole().toString()))
+                .collect(toList());
+        System.out.println(flatMap);
+
+
+        List<Integer> intsList = users.stream()
+                .flatMapToInt(user -> IntStream.of(
+                        user.getId().intValue(),
+                        user.getRole().ordinal(),
+                        user.getName().length()
+                ))
+//                .mapToObj(i -> (new Integer(i)))
+                .mapToObj(Integer::new)
+                .collect(toList());
+
+        System.out.println(intsList);
+    }
+
+    @Test
+    public void collectTest() {
+//        <R> R collect(Supplier<R> supplier,
+//                BiConsumer<R, ? super T> accumulator,
+//                BiConsumer<R, R> combiner);
+//supplier - порождает контейнер в который будут накапливаться данные
+//        accumulator(BiConsumer) - показует как добавлять единичные элементы в конейнер
+//        combiner - показует как будут обьединяться между собой два контейнера(возможно после параллелльных вычислений)
+
+//        позволяет собрать элементы стрима в любой обьект контейнер
+
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        ArrayList<String> names = users.stream()
+                .map(user -> user.getName())
+//                .collect(
+//                        () -> new ArrayList<String>(),
+//                        (container, name) -> container.add(name),
+//                        (container, subConteiner) -> container.addAll(subConteiner)
+//                );
+                .collect(
+                        ArrayList<String>::new,
+                        ArrayList::add,
+                        ArrayList::addAll
+                );
+        System.out.println(names);
+    }
+
+    @Test
+    public void collectWithCollectorTest() {
+//        public static<T, A, R> Collector<T, A, R> of(
+//                Supplier<A> supplier,
+//                BiConsumer<A, T> accumulator,
+//                BinaryOperator<A> combiner,
+//                Function<A, R> finisher,
+//                Collector.Characteristics... characteristics)
+//supplier - порождает контейнер в который будут накапливаться данные
+//        accumulator - добавляет единичные элементы в конейнер
+//        combiner(BinaryOperator) - добавляет все элементы в контейнео
+//        finisher - перепаковывает рабочий контейнер в результат, может быть исключён
+//        позволяет собрать элементы стрима в любой обьект контейнер
+
+        List<User> users = Arrays.asList(
+                new User(1l, "Nika", User.UserrRole.USER),
+                new User(2l, "Mike", User.UserrRole.USER),
+                new User(3l, "Bill", User.UserrRole.USER),
+                new User(4l, "Den", User.UserrRole.USER),
+                new User(5l, "Olya", User.UserrRole.USER));
+
+        LinkedHashSet<String> names = users.stream()
+                .map(user -> user.getName())
+                .collect(Collector.of(
+                        ArrayList<String>::new, //supplier
+                        List::add, //accumulator
+                        (container, subContainer) -> {//combiner
+                            container.addAll(subContainer);
+                            return container;
+                        },
+                        (container) -> new LinkedHashSet<String>() {{//finisher
+                            addAll(container);
+                        }},
+                        Collector.Characteristics.CONCURRENT,
+//                        Collector.Characteristics.IDENTITY_FINISH,
+                        Collector.Characteristics.UNORDERED
+
+                        )
+                );
+        System.out.println(names);
+    }
+
+//1h50min
 
 }
